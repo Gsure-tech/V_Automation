3dd8450a2c8402316206e8d96fd1e7335969489f
gsure-tech <Geeescode@>
2025-07-16 15:10:23 +0100

    add support files

---
+++ cypress/support/constants.js
@@ -0,0 +1,8 @@
+export const HEADERS = {
+  VALID_API_KEY: {
+    X_API_KEY: "$2a$10$ynAASh8Cb.eAnvqB.qxSCu9BUXYqyq5uEXZsQ4nbvt2ra.DgzSjIu",
+  },
+  INVALID_API_KEY: {
+    X_API_KEY: "INVALID_API_KEY",
+  },
+};
