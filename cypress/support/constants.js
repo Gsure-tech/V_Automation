585cb9954dec376bf360aa1cafc2b40985b00c86
gsure-tech <Geeescode@>
2025-08-04 11:32:39 +0100

    refactored all code

--- cypress/support/constants.js
+++ cypress/support/constants.js
@@ -1,6 +1,6 @@
 export const HEADERS = {
   VALID_API_KEY: {
-    X_API_KEY: "$2a$10$ynAASh8Cb.eAnvqB.qxSCu9BUXYqyq5uEXZsQ4nbvt2ra.DgzSjIu",
+    X_API_KEY: "$2a$10$dUZEvMHWX.xFEk/YZZyy6.6keQxi0beWEmJhjdNjzMiTiIPAhfBQu",
   },
   INVALID_API_KEY: {
     X_API_KEY: "INVALID_API_KEY",
